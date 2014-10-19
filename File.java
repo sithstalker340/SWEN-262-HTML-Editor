f924d79ee2da1247c834b811babd2ea16e519b2c
Adam Audycki <apa7395@g.rit.edu>
2014-10-19 14:06:22 -0400

    Added  method stubs

--- File.java
+++ File.java
@@ -38,7 +38,6 @@ public class File{
 	
 	public void setMouseStart(){
 	}
-	}
 	
 	
 }
--- FileContent.java
+++ FileContent.java
@@ -1,4 +1,26 @@
 
 public class FileContent {
 	private File activeFile;
+	
+	public void request(){
+		
+	}
+	
+	public void pushCommand(Command cmd){
+	}
+		
+	public void popCommand(){
+	}
+		
+	public int getMouseStart(){
+		return activeFile.getMouseStart();
+	}
+		
+	public int getMouseEnd(){
+		return activeFile.getMouseEnd();
+	}
+		
+	public void setMouseStart(){
+	}
+	
 }
