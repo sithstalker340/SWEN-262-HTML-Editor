292386848b15afcc53834488d3f3bc541c55cc8f
Adam Audycki <apa7395@g.rit.edu>
2014-10-19 14:26:20 -0400

    Added method stubs to FileHandler.

--- FileHandler.java
+++ FileHandler.java
@@ -1,4 +1,28 @@
 
 public class FileHandler {
-
+	private FileContent mainFileContent;
+	
+	public FileHandler(){
+		
+	}
+	
+	public void save(){
+		
+	}
+	
+	public void saveAs(){
+		
+	}
+	
+	public File load(){
+		return null;
+	}
+	
+	public boolean wellFormed(){
+		return true;
+	}
+	
+	public void quit(){
+		
+	}	
 }
