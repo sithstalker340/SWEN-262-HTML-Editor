6b25a13edcd712a66beaef529b7e25eb331efb05
Arron Reed <ajr1644@rit.edu>
2014-10-22 19:47:03 -0400

    Added comments and method stubs to Prompt

--- Prompt.java
+++ Prompt.java
@@ -1,15 +1,44 @@
 
 public class Prompt{
 
-	public Prompt(){
+	//Prompt constructor
+	public Prompt(String message){
 		
 	}
+	
+	String message
 	
 	/**
 	 * Displays a message to the user
 	 * @param message
 	 */
 	private void displayMessage(String message){
+		
+	}
 	
+	/**
+	 * Displays a dialog for two input custom prompt
+	 * Shows message1, text box, message2, text box
+	 * @param message1, message2
+	 */
+	private String[] displayLines2(String message1, String message2){
+		
+	}
+	
+	/**
+	 * Displays a dialog for one input custom prompt
+	 * Shows message, text box
+	 * @param message
+	 */
+	private String displayLines1(String message){
+		
+	}
+	
+	/**
+	 * Displays dialog with custom prompt
+	 * @param message
+	 */
+	private bool displayBool(String message){
+		
 	}
 }
