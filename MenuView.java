33097459fbffcb95015fbe52b088f5d49b741017
Dylan Hogue <dmh6258@rit.edu>
2014-10-22 19:16:59 -0400

    fixing menuview

--- MenuView.java
+++ MenuView.java
@@ -1,5 +1,14 @@
+import javax.swing.JPanel;
+
 //Methods to display and run dropdown menu functions (save, load, button commands)
-public class MenuView {
+public class MenuView extends JPanel
+{
+
+	public void init(MainView parent) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
 
 	/**
 	 * @param args
