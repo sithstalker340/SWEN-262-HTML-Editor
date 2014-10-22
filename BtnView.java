078ad454e72bf0dae98f96dbe76bb6c5336a9f72
Arron Reed <ajr1644@rit.edu>
2014-10-22 18:52:11 -0400

    Removed auto-gen method stubs from View files.

--- BtnView.java
+++ BtnView.java
@@ -4,9 +4,5 @@ public class BtnView {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	
 }
--- MenuView.java
+++ MenuView.java
@@ -4,9 +4,5 @@ public class MenuView {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 
 }
