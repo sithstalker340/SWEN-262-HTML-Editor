073be85af913ae1a3475a7e87c78ea557a3f273d
Andrew Vogler <avogler2012@yahoo.com>
2014-10-26 18:42:37 -0400

    Trying to get the stash to work

--- FileHandler.java
+++ FileHandler.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Stack;
+
 
 public class FileHandler {
 	private FileContent mainFileContent;
@@ -44,10 +47,70 @@ public class FileHandler {
 	
 	/**
 	 * Checks to see if the text in the current file is valid HTML
-	 * @return bool
+	 * @return boolean
 	 */
 	public boolean wellFormed(){
+		Stack<String> tagStack = new Stack<String>();
+		String allText = mainFileContent.toString(); // the buffer from the file
+		
+		int start = allText.indexOf('<');
+		int end = allText.indexOf('>');
+		String tag = "";
+		
+		if(start == -1 || end == -1){
+			if(start == -1 && end == -1){
+				return true;
+			}
+			
+			return false;
+		}
+		
+		while(start != -1 && end != -1){
+			tag = allText.substring(start + 1, end);
+			
+			if(tag.indexOf(0) == '/'){
+				tag = tag.substring(1);
+				
+				if(tagStack.peek() == tag){
+					tagStack.pop();
+				}
+				
+				else{
+					return false;
+				}
+			}
+			
+			else{
+				int endIndex = allText.indexOf(' ');
+				if(endIndex == -1){
+					tag = tag.substring(0, endIndex);
+				}
+				
+				if(tag.indexOf(0) != '!'){
+					tagStack.push(tag);
+				}
+			}
+			
+			allText = allText.substring(end + 1);
+			start = allText.indexOf('<');
+			end = allText.indexOf('>');
+		}
+		
+		if(tagStack.size() != 0 && checksSelfClose(tag)){
+			return false;
+		}
+		
 		return true;
+	}
+	
+	private boolean checksSelfClose(String tag){
+		String[] selfClosing = {"meta", "link", "input"};
+		
+		if(Arrays.asList(selfClosing).indexOf(tag) > -1){
+			return true;
+		}
+		
+		return false;
 	}
 	
 	/**
