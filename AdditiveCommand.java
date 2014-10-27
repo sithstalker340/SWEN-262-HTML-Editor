daf5152d1dd7d48386933d9dbc70bf47f80e7dea
Andrew Vogler <avogler2012@yahoo.com>
2014-10-27 19:55:44 -0400

    Added untested functionality for Apply() and Undo() for Additive and Subtractive Command

--- AdditiveCommand.java
+++ AdditiveCommand.java
@@ -1,13 +1,28 @@
 
 public class AdditiveCommand extends Command{
 	
-	public boolean isUndoable;
+	private String text;
+	private int startPos;
+	private int endPos;
 	
-	public void Apply(){
+	
+	public AdditiveCommand(String textString, int startPosition, int endPosition){
+		text = textString;
+		startPos = startPosition;
+		endPos = endPosition;
 		
+		if(endPos == startPos){
+			endPos = startPos + text.length();
+		}
 	}
 	
-	public void Undo(){
-		
+	public void Apply(File file){
+		String buffer = file.getBuffer();
+		file.setBuffer(buffer.substring(0,startPos) + text + buffer.substring(endPos));
+	}
+	
+	public void Undo(File file){
+		String buffer = file.getBuffer();
+		file.setBuffer(buffer.substring(0, startPos) + buffer.substring(endPos));
 	}
 }
--- SubtractiveCommand.java
+++ SubtractiveCommand.java
@@ -1,13 +1,23 @@
 
 public class SubtractiveCommand extends Command{
 	
-	public boolean isUndoable;
+	private String text;
+	private int startPos;
+	private int endPos;
 	
-	public void Apply(){
-		
+	public SubtractiveCommand(String textString, int startPosition, int endPosition){
+		text = textString;
+		startPos = startPosition;
+		endPos = endPosition;
 	}
 	
-	public void Undo(){
-		
+	public void Apply(File file){
+		String buffer = file.getBuffer();
+		file.setBuffer(buffer.substring(0, startPos) + buffer.substring(endPos));
+	}
+	
+	public void Undo(File file){
+		String buffer = file.getBuffer();
+		file.setBuffer(buffer.substring(0,startPos) + text + buffer.substring(endPos));
 	}
 }
