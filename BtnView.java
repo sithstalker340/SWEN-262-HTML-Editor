a903f6348c34f3156656018e1d34c476b022c6e5
Dylan Hogue <dmh6258@rit.edu>
2014-10-27 19:56:47 -0400

    Edit btnView and Started on implementing ViewListener

--- BtnView.java
+++ BtnView.java
@@ -9,30 +9,49 @@ public class BtnView extends JPanel
 	JButton btnA;//<a>
 	JButton btnB;//<b>   (bold)
 	JButton btnI;//<i>   (italics)
-	//JButton btnBr;//<br>
 	JButton btnHeader;//<header>
-	//JButton btnLi;//<li>   (list item)
 	JButton btnOl;//<ol>   (ordered list)
 	JButton btnUl;//<ul>   (unordered list)
 	JButton btnDl;//<dl>   (dictionary list)
 	JButton btnTable;//<Table>
 	JButton btnImg;//<img>   (Image)
 	
+	ViewListener vListener;
+	
 	public BtnView(){}
 	
 	public void init(MainView parent, ViewListener listener) 
 	{
+		vListener = listener;
 		this.setSize(parent.getWidth(), 25);
 		this.setLayout(new FlowLayout());
 		this.add(btnB = new JButton("<b>"));
+		//add actionListener for btnB
+		btnB.addActionListener(vListener);
 		this.add(btnI = new JButton("<i>"));
+		//add actionListener for btnI
+		btnI.addActionListener(vListener);
 		this.add(btnA = new JButton("<a>"));
+		//add actionListener for btnA
+		btnA.addActionListener(vListener);
 		this.add(btnHeader = new JButton("<Header>"));
+		//add actionListener for btnHeader
+		btnHeader.addActionListener(vListener);
 		this.add(btnOl = new JButton("<ol>"));
+		//add actionListener for btnOl
+		btnOl.addActionListener(vListener);
 		this.add(btnUl = new JButton("<ul>"));
+		//add actionListener for btnUl
+		btnUl.addActionListener(vListener);
 		this.add(btnDl = new JButton("<dl>"));
+		//add actionListener for btnDl
+		btnDl.addActionListener(vListener);
 		this.add(btnTable = new JButton("<table>"));
+		//add actionListener for btnTable
+		btnTable.addActionListener(vListener);
 		this.add(btnImg = new JButton("<img>"));
+		//add actionListener for btnImg
+		btnImg.addActionListener(vListener);
 		
 		this.setVisible(true);
 	}
--- ViewListener.java
+++ ViewListener.java
@@ -1,6 +1,9 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JMenuItem;
+
 //Methods to get and relay updates to the other view files
 public class ViewListener implements ActionListener
 {
@@ -10,8 +13,33 @@ public class ViewListener implements ActionListener
 	@Override
 	public void actionPerformed(ActionEvent arg0) 
 	{
-		//Exit menu button
-		//if(arg0.getSource() == Me)
+		//ButtonView
+		if(arg0.getSource().getClass().isAssignableFrom((new JButton()).getClass()))
+		{
+			String txt = ((JButton) arg0.getSource()).getText();
+			System.out.println(((JButton) arg0.getSource()).getText());
+			if(txt == "<b>")
+			{
+				System.out.println("BOLD TAG");
+			}
+			else if(txt == "<i>")
+			{
+				System.out.println("ITALICS TAG");
+			}
+			else if(txt == "<a>")
+			{
+				System.out.println("LINK TAG");
+			}
+			else if(txt == "<Header>")
+			{
+				System.out.println("HEADER TAG");
+			}
+		}
+		//MenuView
+		if(arg0.getSource().getClass().isAssignableFrom((new JMenuItem()).getClass()))
+		{
+			System.out.println(((JMenuItem) arg0.getSource()).getText());
+		}
 		
 	}
 
