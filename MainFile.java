60a4d4b0a152a62286fffc0487eb1f7a96dc3800
Adam Audycki <apa7395@g.rit.edu>
2014-10-28 18:42:35 -0400

    Tried different test.

--- MainFile.java
+++ MainFile.java
@@ -14,7 +14,7 @@ public class MainFile
 		input = new InputHandler();
 		//mediator.fileHandler.load("C:\\Users\\Adam\\Desktop\\TEMP_Important\\SWEN-262\\Editor 2\\test.txt");
 		mediator.fileHandler.save();
-		mediator.fileHandler.load("C:\\Users\\Adam\\Desktop\\TEMP_Important\\SWEN-262\\Editor 2\\test.txt");
+		mediator.fileHandler.load("C:\\Users\\Adam\\Desktop\\test1.html");
 		
 	}
 	
