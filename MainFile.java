d7f1351cc151ee3298fce215a49f616adaf826fc
Andrew Vogler <avogler2012@yahoo.com>
2014-10-31 09:00:47 -0400

    Cleaned up mainFile class

--- MainFile.java
+++ MainFile.java
@@ -3,23 +3,17 @@ public class MainFile
 {
 	MenuView menu;
 	MainView view;
-	//ViewListener listener;
 	Mediator mediator;
 	InputHandler input;
+	
 	public MainFile()
 	{
 		mediator = new Mediator();
 		view = new MainView(mediator);
-		
 		input = new InputHandler();
-		//mediator.fileHandler.load("C:\\Users\\Adam\\Desktop\\TEMP_Important\\SWEN-262\\Editor 2\\test.txt");
-		//mediator.fileHandler.save();
-		//mediator.fileHandler.load("C:\\Users\\Adam\\Desktop\\test1.html");
-
-		
 	}
 	
 	public static void main(String args[]){
-		MainFile main = new MainFile();
+		new MainFile();
 	}
 }
