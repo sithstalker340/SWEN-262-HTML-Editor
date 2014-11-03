5479516fbe6c192086b1689d7a04fd265b4612c1
Adam Audycki <apa7395@g.rit.edu>
2014-11-02 21:40:01 -0500

    Added InsertLink functionality.

--- InsertLinkCommand.java
+++ InsertLinkCommand.java
@@ -1,16 +1,30 @@
 
 public class InsertLinkCommand extends Command {
-
-	@Override
-	public void Apply(File file) {
-		// TODO Auto-generated method stub
-		
+	
+	private String text;
+	private int startPos;
+	private int endPos;
+	private String buffer;
+	
+	public InsertLinkCommand(String textString, int startPosition, int endPosition){
+		text = textString;
+		startPos = startPosition;
+		endPos = endPosition;
 	}
-
-	@Override
-	public void Undo(File file) {
-		// TODO Auto-generated method stub
-		
+	
+	/**
+	 * Updates a file's text by replacing
+	 * it with the text + a desired substring
+	 */
+	public void Apply(File file){
+		buffer = file.getBuffer();
+		file.setBuffer(buffer.substring(0,startPos) + "<a href=" + "\"" + text + "\"" + ">" + buffer.substring(startPos,endPos) + "</a>"+ buffer.substring(endPos));
 	}
-
+	
+	/**
+	 * Undoes the addition of text from a file
+	 */
+	public void Undo(File file){
+		file.setBuffer(buffer);
+	}
 }
