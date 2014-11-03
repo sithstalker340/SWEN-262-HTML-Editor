cf5afcd7a58a196cb196e6322a1515fe2c1854cc
Andrew Vogler <avogler2012@yahoo.com>
2014-11-02 19:22:36 -0500

    Added functionality for display message, taking in 1 line of user input, taking in a user choice

--- PromptManager.java
+++ PromptManager.java
@@ -1,14 +1,48 @@
+import java.awt.Dialog;
+import java.awt.Frame;
+import java.awt.GridLayout;
+import java.awt.Label;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 public class PromptManager{
 	
 	String message;
+	MainView mainView;
+	JFrame parent;
 	
 	//PromptHandler constructor
-	public PromptManager(){
+	public PromptManager(MainView m){
 		message = "";
+		mainView = m;
 	}
 	
-	public void setMessage(String m){
+	public void createNewPrompt(int options, String m){
 		message = m;
+		
+		switch(options){
+		case 0: // no user feedback
+				displayMessage(m);
+				System.out.println("0 selected");
+			break;
+			
+		case 1: // user selects yes or no
+				displayBool(m);
+			break;
+			
+		case 2: // user enters 1 string
+				displayLines1(m);
+			break;
+		case 3: // user enters 2 strings
+				
+			break;
+		}
 	}
 	
 	/**
@@ -16,7 +50,27 @@ public class PromptManager{
 	 * @param message
 	 */
 	private void displayMessage(String m){
+		parent = new JFrame();
 		
+		JOptionPane pane = new JOptionPane();
+        pane.setMessage(m);
+        
+        JButton ok = new JButton("Ok");
+        Object[] options = {ok};
+        pane.setOptions(options);
+        
+        ok.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e){
+        		returnBoolPrompt("true");
+        		parent.dispose();
+        	}
+        });
+        
+        parent.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        parent.add(pane);
+        parent.pack();
+        parent.setLocationRelativeTo(null);
+		parent.setVisible(true);	
 	}
 	
 	/**
@@ -34,15 +88,66 @@ public class PromptManager{
 	 * @param message
 	 */
 	private String displayLines1(String m){
-		return null;
+		JPanel panel = new JPanel(new GridLayout(2,1));
+		Label label = new Label(m);
+		Object[] options = {"Yes", "No"};
+		JTextField textField = new JTextField();
+		panel.add(label);
+		panel.add(textField);
+		
+		int result = JOptionPane.showOptionDialog(null, panel, "Testing", JOptionPane.OK_CANCEL_OPTION,
+				JOptionPane.QUESTION_MESSAGE, null, options, null);
+		if(result == JOptionPane.OK_OPTION){
+			System.out.println(textField.getText());
+			return textField.getText();
+		}
+		else return "";
 	}
 	
 	/**
 	 * Displays dialog with custom prompt
 	 * @param message
 	 */
-	private Boolean displayBool(String m){
-		return null;
+	private void displayBool(String m){
+		parent = new JFrame();
+		
+		JOptionPane pane = new JOptionPane();
+        pane.setMessage(m);
+        JButton yes = new JButton("Yes");
+        JButton no = new JButton("No");
+        Object[] options = {yes, no};
+        pane.setOptions(options);
+        
+        yes.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e){
+        		returnBoolPrompt("true");
+        		parent.dispose();
+        	}
+        });
+        
+        no.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e){
+        		returnBoolPrompt("false");
+        		parent.dispose();
+        	}
+        });
+        
+        parent.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        parent.add(pane);
+        parent.pack();
+        parent.setLocationRelativeTo(null);
+		parent.setVisible(true);	
+		parent.setFocusable(true);
+	}
+	
+	public boolean returnBoolPrompt(String b){
+		System.out.println(b);
+		
+		if(b == "true"){ 
+			return true;
+		}
+		
+		else return false;
 	}
 }
 
