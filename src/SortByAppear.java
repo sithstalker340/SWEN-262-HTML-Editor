700d5336e72eb2d66e965d67a0d011677c8d3c73
Adam Audycki <apa7395@g.rit.edu>
2014-11-04 23:50:22 -0500

    Only prints the URL. Does not show the number of occurrences for Alpha Sort.

--- src/SortByAppear.java
+++ src/SortByAppear.java
@@ -11,14 +11,22 @@ public class SortByAppear implements LinkedViewStrategy{
 		urlList = new ArrayList<String>();
 	}
 
-	public List<String> parse(String buffer) {
+	public List<String> parse(String buffer){
 		splitText = buffer.split("[\n]+");
 		String[] tempList;
+		String[] tempList2;
 		for(int i = 0;i < splitText.length;i++){
 			tempList = splitText[i].split("<a href=+");
 			for(int j = 0;j<tempList.length;j++){
-				urlList.add(tempList[j]);
+				if(tempList[j].startsWith("\"")){
+					urlList.add(tempList[j]);
+				}
 			}
+		}
+		int end;
+		for(int i = 0;i < urlList.size();i++){
+			end = urlList.get(i).indexOf("></a>");
+			urlList.set(i, urlList.get(i).substring(0,end));
 		}
 		
 		return urlList;
