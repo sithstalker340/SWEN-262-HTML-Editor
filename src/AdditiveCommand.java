e14f0acb0651d441efe3ab5c5b3be97d5aca058a
braxton frederick <baf9201@rit.edu>
2014-11-06 00:55:28 -0500

    Fixed the error where additive commands would duplicate contents of the buffer semi-randomly

--- src/AdditiveCommand.java
+++ src/AdditiveCommand.java
@@ -20,21 +20,12 @@ public class AdditiveCommand extends Command{
 	public void Apply(File file){
 		String b = file.getBuffer();
 		
-		if(b.length() == 0 || end >= b.length() ){
-			//replace file buffer with newer one
-			file.setBuffer(text);
+		if(text == b){
+			return; //buffers are the same
+		}else{
 			isUndoable = true;
 			buffer = b;
-		}else{
-			String newBuffer = b.substring(0,start) + text + b.substring(end);
-			
-			if(newBuffer != b){ 
-				file.setBuffer(newBuffer); //update buffer
-				this.isUndoable = true; //buffers are different, save the change
-				this.buffer = b;
-			}else{ 
-				return; //buffers are the same don't bother
-			}
+			file.setBuffer(text);
 		}
 	}
 	
