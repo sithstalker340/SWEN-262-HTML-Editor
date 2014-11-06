7957bf6670e511b0630dc822ff9fda051fa91a88
braxton frederick <baf9201@rit.edu>
2014-11-06 02:52:38 -0500

    updated how insert list stores the list string Mediator now passes the correct value of cursor start to commands

--- src/InsertListCommand.java
+++ src/InsertListCommand.java
@@ -1,39 +1,31 @@
 
 public class InsertListCommand extends Command {
-	private int numberRows;
 	
-	public InsertListCommand(String textString, int start, int numRows){
-		text = textString;
-		start = start;
-		numberRows = numRows;
+	public InsertListCommand(String tag, int start, int numRows){
+		text = "";
+		text += '<' + tag + '>';
+		
+		for(int i = 0; i < numRows; i++){
+			text += '\n';
+			
+			if(tag == "dl"){
+				text += "<dt> </dt>" + '\n' + "<dd> </dd>";
+			}else{
+				text += "<li> </li>";				
+			}
+		}
+		
+		text += '\n' + "<" + tag + ">";	
+		
+		this.start = start;
 		isUndoable = true;
 	}
 	
 	public void Apply(File file) {
 		buffer = file.getBuffer();
 		
-	    StringBuilder builder = new StringBuilder();
-	    builder.append("\n");
-	    builder.append("<" + text + ">");
+	    String newBuffer = buffer.substring(0,start) + text + buffer.substring(start);
 	    
-		for(int i = 0; i < numberRows; i++){
-			if(text == "dl"){
-				builder.append('\n');
-				builder.append("<dt>" + " " + "</dt>"); 
-		        builder.append('\n');
-		        builder.append("<dd>"  + " " + "</dd>");   
-		    }
-
-		    else{
-		    	builder.append('\n');
-		    	builder.append("<li>" + " " + "</li>");
-		    }       
-		}
-		
-		builder.append('\n');
-	    builder.append("</" + text + ">");
-	    
-	    String newBuffer = buffer.substring(0,start) + builder.toString() + buffer.substring(start);
 		file.setBuffer( newBuffer );
 	}
 	
