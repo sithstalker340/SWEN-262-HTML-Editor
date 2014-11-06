f006a47af75e7d1c9f716aa8a601f85884156484
Adam Audycki <apa7395@g.rit.edu>
2014-11-05 20:34:38 -0500

    Added some stuff for the previewer. Added the temporary  testbird.jpg

--- src/ImagePreviewer.java
+++ src/ImagePreviewer.java
@@ -8,38 +8,26 @@ import java.io.File;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
-
-	/**
-	 * This class demonstrates how to load an Image from an external file
-	 */
-	public class ImagePreviewer extends Component {
-	           
-	    BufferedImage img;
-	 
-	    public void paint(Graphics g) {
-	        g.drawImage(img, 0, 0, null);
-	    }
-	 
-	    public ImagePreviewer(String path){
-	       try {
-	           img = ImageIO.read(new File(path));
-	       } catch (IOException e) {
-	       }
-	       
-	       JFrame f = new JFrame("Load Image Sample");
+@SuppressWarnings("serial")
+public class ImagePreviewer extends Component {
            
-	 
-	        f.add(this);
-	        f.pack();
-	        f.setVisible(true);
-	 
-	    }
-	 
-	    public Dimension getPreferredSize() {
-	        if (img == null) {
-	             return new Dimension(100,100);
-	        } else {
-	           return new Dimension(img.getWidth(null), img.getHeight(null));
-	       }
-	    }
-	}
+    BufferedImage img;
+ 
+    public void paint(Graphics g) {
+        g.drawImage(img, 0, 0, null);
+    }
+ 
+    public ImagePreviewer(String path){
+       try {
+           img = ImageIO.read(new File(path));
+       }catch (IOException e){
+    	   e.printStackTrace();
+       }
+       
+       JFrame f = new JFrame("Load Image Sample");
+ 
+       f.add(this);
+       f.pack();
+       f.setVisible(true);
+    }
+}
